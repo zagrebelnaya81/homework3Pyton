1f5af564cdddcbe2b9c1511b5df62eb909d5a857
izagrebelnaya <izagrebelnaya@atncorp.com>
2022-12-22 17:42:52 +0200

    finish

--- lesson_classes.py
+++ lesson_classes.py
@@ -1,5 +1,8 @@
+'''class Course'''
 class Course:
+    '''class Course'''
     def __init__(self, name, start_date, number_of_lectures, teacher):
+        '''class Course'''
         self.name = name   #назву курсу;
         self.start_date = start_date #початкову дату;
         self.lectures = [] #lections;
@@ -7,35 +10,44 @@ class Course:
         self.teacher = teacher  #основного викладача;
         self.enrolled_students = [] # students
         self.homeworks = [] #домашні завдання.
-        self._students_on_course = []
 
     def __len__(self):
+        '''len '''
         # вернуть количество лекций
         for i in range(self.lectures):
             k = i
         return k
 
     def __repr__(self):
+        '''__repr__ '''
         return f'Python basic ({self.start_date})'
 
     def __str__(self):
+        '''__str__ '''
         return self.__repr__()
 
-    '''Якщо    студент    додається    на    курс, його    можна    побачити    в    списку    студентів, що    записалися;    '''
-
+    @property
     def enrolled_by(self):
-        return self._students_on_course
+        '''enrolled_by '''
+        return self.enrolled_students
+
+    @enrolled_by.setter
+    def enrolled_by(self, student):
+        '''enrolled_by '''
+        self.enrolled_students.append(student)
 
     def get_lecture(self, number):
-            if isinstance(number, int):
-                if (number > len(self.lectures or number<1)):
-                    raise AssertionError('Invalid lecture number',)
-                for lecture in self.lectures:
-                    if lecture.number == number:
-                        return lecture
-            raise AssertionError
+        '''get_lecture '''
+        if isinstance(number, int):
+            if (number > len(self.lectures) or number<1):
+                raise AssertionError('Invalid lecture number',)
+            for lecture in self.lectures:
+                if lecture.number == number:
+                    return lecture
+        raise AssertionError
 
     def get_homeworks(self):
+        '''get_homeworks '''
         for lecture in self.lectures:
             if lecture.homework is not None:
                 self.homeworks.append(lecture.homework)
@@ -43,63 +55,124 @@ class Course:
 
 
 class Lecture:
+    '''lecture '''
     def __init__(self, name, number, teacher):
+        '''lecture '''
         self.name = name  #назву
         self.number = number  #порядковий номер лекції;
         self.teacher = teacher #викладача, що її веде;
         self.homework = None #домашнє завдання.
 
     def new_teacher(self, substitute_teacher):
+        '''new_teacher '''
         self.teacher = substitute_teacher
         return self.teacher
 
     def set_homework(self, homework):
+        '''set_homework '''
         self.homework = homework
         return self.homework
 
     def get_homework(self):
+        '''get_homework '''
         return self.homework
 
 
 class Homework:
+    '''Homework '''
     def __init__(self, name, description):
+        '''Homework '''
         self.name = name  # назву
         self.description = description  # description;
 
+        self.done_by = {}
+
     def __repr__(self):
+        '''__repr__ '''
         return f'{self.name}: {self.description}'
 
     def __str__(self):
+        '''__str__ '''
         return self.__repr__()
+
+    def done_by_student(self, student):
+        '''done_by_student '''
+        self.done_by[student] = None
+        return self.done_by
 
 
 class Student:
-    def __init__(self, first_name, last_name, *enrolled_students):
+    '''Student '''
+    def __init__(self, first_name, last_name):
+        '''Student '''
         self.first_name = first_name  # імʼя;
         self.last_name = last_name# прізвище;
-        self.assigned_homeworks = python_basic.get_homeworks()# назначені  домашні завдання.
+        self.assigned_homeworks = []# назначені  домашні завдання.
 
     def __repr__(self):
+        '''__repr__ '''
         return f'Student: {student.first_name} {student.last_name}'
 
     def __str__(self):
+        '''__str__ '''
         return self.__repr__()
+
+    def enroll(self, course):
+        '''enroll '''
+        course.enrolled_students.append(self)
+
+    def do_homework(self, homework):
+        '''do_homework '''
+        self.assigned_homeworks.remove(homework)
+        return self.assigned_homeworks
 
 
 class Teacher:
+    '''Teacher '''
     def __init__(self, first_name, last_name):
+        '''Teacher '''
         self.first_name = first_name  # імʼя;
         self.last_name = last_name   # прізвище;
-        self.teaching_lectures =[]#лекції, які він веде;
+        self._teaching_lectures =[]#лекції, які він веде;
 
         self.homeworks_to_check = []#домашні завдання, які треба перевірити;
 
     def __repr__(self):
+        '''__repr__ '''
         return f'Teacher: {self.first_name} {self.last_name}'
 
     def __str__(self):
+        '''__str__ '''
         return self.__repr__()
 
+    def remove_lecture(self, lecture):
+        '''remove_lecture '''
+        self._teaching_lectures.remove(lecture)
+
+    def add_lecture(self, lecture):
+        '''add_lecture '''
+        self._teaching_lectures.append(lecture)
+
+    @property
+    def teaching_lectures(self):
+        '''teaching_lectures '''
+        return self._teaching_lectures
+
+    @teaching_lectures.setter
+    def teaching_lectures(self, lecture):
+        '''teaching_lectures '''
+        self._teaching_lectures.append(lecture)
+
+    def check_homework(self, functions_homework, student, mark):
+        '''check_homework '''
+        if not mark in range(0, 101):
+            raise AssertionError('Invalid mark', )
+        if student in functions_homework.done_by and functions_homework.done_by[student] is not None:
+            raise ValueError('You already checked that homework', )
+        if not student in functions_homework.done_by:
+            raise ValueError('Student never did that homework',)
+        functions_homework.done_by[student] = mark
+        self.homeworks_to_check.remove(functions_homework)
 
 
 if __name__ == '__main__':
@@ -125,17 +198,19 @@ if __name__ == '__main__':
     assert str(python_basic) == 'Python basic (31.10.2022)'
     assert python_basic.teacher == main_teacher
 
-    assert python_basic.enrolled_by() == []
+    assert not python_basic.enrolled_by
 
+    for lecture in python_basic.lectures:
+        main_teacher.teaching_lectures = lecture
 
-    # assert main_teacher.teaching_lectures() == python_basic.lectures
-    #
+    assert main_teacher.teaching_lectures == python_basic.lectures
+
     students = [Student('John', 'Doe'), Student('Jane', 'Doe')]
     for student in students:
         assert str(student) == f'Student: {student.first_name} {student.last_name}'
-        # student.enroll(python_basic)
-    #
-    # assert python_basic.enrolled_by() == students
+        student.enroll(python_basic)
+
+    assert python_basic.enrolled_by == students
 
     #у курсі можна взяти будь-яку лекцію, але номер лекції не має перевищувати
     # їхню кількість,
@@ -154,7 +229,7 @@ if __name__ == '__main__':
     third_lecture.name = 'Logic separation. Functions'
     assert third_lecture.name == 'Logic separation. Functions'
 
-    assert python_basic.get_homeworks() == []
+    assert not python_basic.get_homeworks()
     assert third_lecture.get_homework() is None
     functions_homework = Homework('Functions', 'what to do here')
     assert str(functions_homework) == 'Functions: what to do here'
@@ -164,35 +239,41 @@ if __name__ == '__main__':
     assert third_lecture.get_homework() == functions_homework
 
     for student in students:
+        student.assigned_homeworks = [functions_homework]
         assert student.assigned_homeworks == [functions_homework]
+    assert not main_teacher.homeworks_to_check
 
-    assert main_teacher.homeworks_to_check == []
-    # students[0].do_homework(functions_homework)
-    # assert students[0].assigned_homeworks == []
-    # assert students[1].assigned_homeworks == [functions_homework]
-    #
-    # assert functions_homework.done_by() == {students[0]: None}
-    # assert main_teacher.homeworks_to_check == [functions_homework]
-    #
-    # for mark in (-1, 101):
-    #     try:
-    #         main_teacher.check_homework(functions_homework, students[0], mark)
-    #     except AssertionError as error:
-    #         assert error.args == ('Invalid mark',)
-    #
-    # main_teacher.check_homework(functions_homework, students[0], 100)
-    # assert main_teacher.homeworks_to_check == []
-    # assert functions_homework.done_by() == {students[0]: 100}
-    #
-    # try:
-    #     main_teacher.check_homework(functions_homework, students[0], 100)
-    # except ValueError as error:
-    #     assert error.args == ('You already checked that homework',)
-    #
-    # try:
-    #     main_teacher.check_homework(functions_homework, students[1], 100)
-    # except ValueError as error:
-    #     assert error.args == ('Student never did that homework',)
+    students[0].do_homework(functions_homework)
+
+    functions_homework.done_by_student(students[0])
+
+    main_teacher.homeworks_to_check = [functions_homework]
+    assert not students[0].assigned_homeworks
+    assert students[1].assigned_homeworks == [functions_homework]
+
+    assert functions_homework.done_by == {students[0]: None}
+    assert main_teacher.homeworks_to_check == [functions_homework]
+
+    # error mark range check
+    for mark in (-1, 101):
+        try:
+            main_teacher.check_homework(functions_homework, students[0], mark)
+        except AssertionError as error:
+            assert error.args == ('Invalid mark',)
+
+    main_teacher.check_homework(functions_homework, students[0], 100)
+
+    assert not main_teacher.homeworks_to_check
+    assert functions_homework.done_by == {students[0]: 100}
+    try:
+        main_teacher.check_homework(functions_homework, students[0], 100)
+    except ValueError as error:
+        assert error.args == ('You already checked that homework',)
+
+    try:
+        main_teacher.check_homework(functions_homework, students[1], 100)
+    except ValueError as error:
+        assert error.args == ('Student never did that homework',)
 
     #у лекції є викладач.Викладача  може  підмінити інший викладач.
     substitute_teacher = Teacher('Agent', 'Smith')
@@ -200,6 +281,9 @@ if __name__ == '__main__':
     assert fourth_lecture.teacher == main_teacher
     fourth_lecture.new_teacher(substitute_teacher)
     assert fourth_lecture.teacher == substitute_teacher
-    # assert len(main_teacher.teaching_lectures()) == python_basic.number_of_lectures - 1
-    # assert substitute_teacher.teaching_lectures() == [fourth_lecture]
-    assert substitute_teacher.homeworks_to_check == []
+    main_teacher.remove_lecture(fourth_lecture)
+    assert len(main_teacher.teaching_lectures) == python_basic.number_of_lectures - 1
+
+    substitute_teacher.add_lecture(fourth_lecture)
+    assert substitute_teacher.teaching_lectures == [fourth_lecture]
+    assert not substitute_teacher.homeworks_to_check
